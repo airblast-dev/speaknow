b7835421bf1f0df274b207374ae673dac2880453
Tayfun Bocek <tayfunbocek@live.ca>
2025-07-16 15:31:45 +0300

    ensure size and alignment guarantees for SnMessage

--- components/speaknow/include/speaknow.h
+++ components/speaknow/include/speaknow.h
@@ -1,4 +1,5 @@
 #include "esp_now.h"
+#include <stdalign.h>
 #include <stdint.h>
 
 enum SnVersion {
@@ -9,13 +10,19 @@ enum SnVersion {
   { 0x68, 0x87, 0x00, 0x12, 0x01, 0x34, 0x82, 0xFF }
 const static uint8_t SN_MAGIC[8] = SN_MAGIC_BYTES;
 
-struct SnMessage {
+struct __attribute__((aligned(alignof(uint32_t)), packed)) SnMessage {
   uint32_t capacity;
+  uint32_t data_len;
   const uint8_t magic[8];
   const enum SnVersion version;
-  uint32_t data_len;
   uint8_t data[] __attribute__((counted_by(data_len)));
 };
+_Static_assert(sizeof(struct SnMessage) == sizeof(uint32_t) * 2 +
+                                               sizeof(uint8_t[8]) +
+                                               sizeof(enum SnVersion),
+               "bad speaknow message size");
+_Static_assert(alignof(struct SnMessage) == alignof(uint32_t),
+               "bad speaknow message alignment");
 
 // The maximum number of bytes than can be sent in a single message
 static const uint32_t SN_MAX_MESSAGE_LEN =
